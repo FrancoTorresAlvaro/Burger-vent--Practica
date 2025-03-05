bebb75727344c67994c314db89239423994f6539
Brandon-2git <cbi2173048650@izt.uam.mx>
2025-03-04 22:55:28 -0600

    Commit inicial

---
+++ src/main/java/com/burguerVent/burguerVent/BurguerVentApplication.java
@@ -0,0 +1,13 @@
+package com.burguerVent.burguerVent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurguerVentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurguerVentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burguerVent/burguerVent/BurguerVentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burguerVent.burguerVent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurguerVentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
