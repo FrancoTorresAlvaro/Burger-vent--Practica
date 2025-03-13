3edc4fc4cd0eaed9f9ee372dabe7de81849e7b7c
FrancoTorresAlvaro <cbi2173013228@izt.uam.mx>
2025-03-13 10:37:04 -0600

    Actulizacion de las clases necesarias

---
+++ src/main/java/com/burguerVent/negocio/modelo/Pedido.java
@@ -0,0 +1,11 @@
+package com.burguerVent.negocio.modelo;
+
+import java.util.ArrayList;
+
+public class Pedido {
+	private float total;
+	private ArrayList<Productos> listaProductos=new ArrayList<>();
+	
+	
+
+}
